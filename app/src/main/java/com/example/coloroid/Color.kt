8ebe7597067568f901bf37cc51a50d1a07d11e0f
Ghostik13 <roxan95@mail.ru>
2021-03-17 17:03:31 +0300

    firstCommit

--- app/src/main/java/com/example/coloroid/Color.kt
+++ app/src/main/java/com/example/coloroid/Color.kt
@@ -1,4 +1,7 @@
 package com.example.coloroid
 
-data class Color (var color: Int, var name: String, var hex: String){
-}
+data class Color (
+    var color: Int,
+    var name: String,
+    var hex: String
+)
